db484ac7ed7f2172fa6e7b2c23dde9eaf49c6c32
EGerritsen99 <145434680+EGerritsen99@users.noreply.github.com>
2023-09-19 09:58:02 +0200

    Add files via upload

---
+++ phonebill/PhoneBillCalculatorRedo.java
@@ -0,0 +1,12 @@
+package phonebill;
+
+public class PhoneBillCalculatorRedo {
+
+
+    public static void main(String args[]) {
+
+        PhoneBill bill = new PhoneBill(123456);
+        bill.setMinutesUsed(1000);
+        bill.printItemizedBill();
+    }
+}
